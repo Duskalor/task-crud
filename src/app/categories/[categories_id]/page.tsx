d6c8c49934ebf4b96340082f8e17d1543bbca9a3
duskalor <duskalor1@gmail.com>
2025-02-18 11:54:04 -0500

    apge

--- src/app/categories/[categories_id]/page.tsx
+++ src/app/categories/[categories_id]/page.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import { TaskTable } from './components/TaskTable';
 export default async function Page({ params }: { params: Params }) {
   const { categories_id } = await params;
+  if (!categories_id) return <div>No categories_id found</div>;
   const supabase = await createClient();
   const { data: tasks } = await supabase
     .from('task')
     .select('*, status(*)')
     .eq('categories_id', categories_id);
+
   if (!tasks) return <div>No tasks found</div>;
   return <TaskTable tasks={tasks} categories_id={String(categories_id)} />;
 }
